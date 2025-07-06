8194f8b52313cd8ba6710548b933e368b360d498
Aloysius <aloysiusee.seejoon@gmail.com>
2025-07-06 18:20:03 +0800

    Lion Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 movement;
-    private float speed = 10f;
+    private float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
