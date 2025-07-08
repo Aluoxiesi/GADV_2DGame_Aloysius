dee60726ecf2767e8206b4809aeec63638f31d0c
Aloysius <aloysiusee.seejoon@gmail.com>
2025-07-08 21:09:10 +0800

    made word list test

--- Assets/Scripts/WordList.cs
+++ Assets/Scripts/WordList.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class WordList : MonoBehaviour
 {
     public string word = "";
+    public List<string> correctwords = new List<string>{"lion"};
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Check()
+    {
+        foreach (string correct in correctwords)
+        {
+            if (word == correct)
+            {
+                Debug.Log("Correct!" + word);
+                word = "";
+                return;
+            }
+           
+           
+        }
+        Debug.Log("Wrong" + word);
+        word = "";
+    }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Check();
+        }
         
     }
 }
