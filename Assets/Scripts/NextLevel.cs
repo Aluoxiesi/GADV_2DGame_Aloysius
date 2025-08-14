5bd9b26e33a22eeeaf31f62aeeecaa9f7f22007d
Aloysius <aloysiusee.seejoon@gmail.com>
2025-08-14 15:58:59 +0800

    Final screen
    -Made it so that when the rescuedanimals count is 2, then the "Next Level" button brings it to a final screen

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -8,8 +8,20 @@ public class NextLevel : MonoBehaviour
     // Start is called before the first frame update
     public void NextLev()
     {
-        SceneManager.LoadScene("Zebra Level");
+        int count = AnimalManager.Instance.rescuedAnimals.Count;
+        if (count == 1)
+        {
+            SceneManager.LoadScene("Zebra Level");
+        }
+        else if (count == 2)
+        {
+            SceneManager.LoadScene("End");
+        }
+        
     }
+
+
+
 
     // Update is called once per frame
     void Update()
